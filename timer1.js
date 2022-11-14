d004c4aca84f1c20932e69cb7572ef25d7cc9cee
Jason Kadman <jkadman231@gmail.com>
2022-11-14 04:22:59 +0000

    after experimenting with a bunch of options, I found a function that helps determine whether something in an array is a number or not.  Decided to use that.

--- timer1.js
+++ timer1.js
@@ -1,30 +1,21 @@
-// const args 
-// console.log(args);
-
-// const beeper = function (interval) {
-  // interval = process.argv.slice(2);
-  // for (let int of interval) {
-    // setTimeout(() => {
-      // console.log('.');
-    // }, int * 1000);
-  // }
-// }
-// 
-// beeper();
-// 
-
 const beeper = function (interval) {
   interval = process.argv.slice(2);
-  if (interval === NaN) {
-    return '';
+  if (onlyNumbers(interval) && (interval < 0 === false)) {
+    interval.forEach(element => {
+      setTimeout(() => {
+        console.log('.')
+      }, element * 1000)
+    })
   } else {
-  interval.forEach(element => {
-    setTimeout(() => {
-      console.log('.')
-    }, element * 1000)
-  })
+    return ''
   }
-  
-}
+};
 
 beeper()
+
+// a function to check if only numbers are submitted to the beeper function
+function onlyNumbers(array) {
+  return array.every(element => {
+    return !isNaN(element);
+  });
+};
